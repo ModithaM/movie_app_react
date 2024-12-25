1234f31507afed59e68da53b981147b4891f0649
Moditha Marasingha <marasinghamoditha51@gmail.com>
2024-12-25 15:30:41 +0530

    main pages done

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,23 +1,38 @@
-function MovieCard({movie}) {
+import "../css/MovieCard.css"
+import { useMovieContext } from "../contexts/MovieContext"
 
-    function addToFav() {
-        alert("Added to Favourites");
+function MovieCard({movie}) {
+    const {isFavorite, addToFavorites, removeFromFavorites} = useMovieContext()
+    const favorite = isFavorite(movie.id)
+
+    function onFavoriteClick(e) {
+        e.preventDefault()
+        if (favorite) removeFromFavorites(movie.id)
+        else addToFavorites(movie)
     }
 
-    return(
-        <div className="movie-card">
-            <div className="movie-poster">
-                <img src={movie.url} alt={movie.title} />
-                <div className="movie-overlay">
-                    <button className="fav-btn" onClick={addToFav}>♡</button>
-                </div>
-            </div>
-            <div className="movie-information">
-                <h3>{movie.title}</h3>
-                <p>{movie.year}</p>
+    const limitText = (text, wordLimit) => { //word limitting function
+        const words = text.split(' ');
+        return words.length > wordLimit
+          ? words.slice(0, wordLimit).join(' ') + '...'
+          : text;
+      };
+
+    return <div className="movie-card">
+        <div className="movie-poster">
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
+            <div className="movie-overlay">
+                <button className={`favorite-btn ${favorite ? "active" : ""}`} onClick={onFavoriteClick}>
+                    ♥
+                </button>
             </div>
         </div>
-    );
+        <div className="movie-info">
+            <h3>{movie.title}</h3>
+            <p>{limitText(movie.overview, 20)}</p>
+            <p>{movie.release_date?.split("-")[0]}</p>
+        </div>
+    </div>
 }
 
-export default MovieCard;
+export default MovieCard
---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "6c0b8d080d4034a20fa5d81c5d0915cf";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+}; //fetch api and get movies
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+}; //search movie using search quary
